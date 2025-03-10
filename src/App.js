2f286586b7de7075e95d6b90f69d2891f82a2959
Berigny <david@berigny.org>
2025-03-10 17:23:47 +1100

    dd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import LoginButton from './LoginButton';
 import LogoutButton from './LogoutButton';
+import Profile from './Profile';
 
 function App() {
   return (
     <div>
       <h1>My App</h1>
+      <Profile />
       <LoginButton />
       <LogoutButton />
     </div>
