8043aba187f71f7f899577efc31b829f9c9c1bb2
Berigny <david@berigny.org>
2025-03-10 21:22:36 +1100

    asdf

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import LoginButton from './LoginButton';
+/*import LoginButton from './LoginButton';
 import LogoutButton from './LogoutButton';
 import Profile from './Profile';
 
@@ -12,5 +12,39 @@ function App() {
     </div>
   );
 }
+
+export default App;*/
+
+import React from "react";
+import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
+
+const App = () => {
+  return (
+    <Auth0Provider
+      domain="your-auth0-domain"
+      clientId="your-auth0-client-id"
+      authorizationParams={{ redirect_uri: window.location.origin }}
+    >
+      <Auth />
+    </Auth0Provider>
+  );
+};
+
+const Auth = () => {
+  const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
+
+  return (
+    <div>
+      {!isAuthenticated ? (
+        <button onClick={() => loginWithRedirect()}>Sign Up / Log In</button>
+      ) : (
+        <>
+          <h3>Welcome, {user.name}!</h3>
+          <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Log out</button>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default App;
