b958e4eb23a0e1e1a90462f000f1153aae737b43
Berigny <david@berigny.org>
2025-03-10 17:09:05 +1100

    ss

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
+
 import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './App';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <Auth0Provider
+    domain="http://dev-lyd8zg4866wjaxih.us.auth0.com"
+    clientId="oGzpVDcNA9n3OYvzTzDtcMVGGLyp0efz"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+    <App />
+  </Auth0Provider>
+);
+
+
+
+/* import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
